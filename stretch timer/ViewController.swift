353716ad1d655601df91f39710054b808c30ae6b
RIMIKA <rimikamimmy727@gmail.com>
2018-04-25 22:25:06 +0900

    最初の画面は全部buttonで押せるようににしてみた

--- stretch timer/ViewController.swift
+++ stretch timer/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var label: UILabel!
+    @IBOutlet var Profilebutton: UIButton!
+    @IBOutlet var Timerbutton: UIButton!
+    @IBOutlet var graphbutton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    @IBAction func ProfilebuttonTapped(){
+        
+        self.performSegue(withIdentifier: "toProfileScene", sender: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
